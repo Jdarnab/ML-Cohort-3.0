a2a7fb98f66e59a0f6ce0665f86e7ee589e76e6b
Joydeep Banik <jdarnab@gmail.com>
2025-01-16 17:28:37 +0600

    Add files via upload

---
+++ Week 1/Video 10 (list. tuple).py
@@ -0,0 +1,36 @@
+# y= (1,2,3,)
+# x= list(y)
+# x.append(14)
+# x.remove(1)
+# print(x)
+# z=tuple(x)
+# print(z)
+#dictionary
+# man= {'name':'Jdarnab'}
+# man['age']=30
+# man= {'name': 'jdarnab',  'age':30}
+# print (man)
+# del man['age']
+# print(man)
+#task on Video 10
+c=[1,2,3,4,5]
+c.append(6)
+c.remove(5)
+# print(c[1])
+print(sum(c)/len(c))
+cities=("Dhk","Ctg", "Com", "Shy", "Bb")
+print(cities[2])
+# print(list(cities))
+y=list(cities)
+y.append("Feni")
+print(tuple(y))
+# print(cities)
+# print(y)
+# print(type(y))
+# print( cities)
+# print(list(cities))
+# print(type(cities))
+z={'Math':80,'Phy':90,'Chem':70}
+z['Eng']=85
+z['Chem']=75
+print(z)
