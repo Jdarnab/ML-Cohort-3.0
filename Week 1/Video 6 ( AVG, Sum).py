b963d5195365a0de7b13cf8cfb55b60d54248d3f
Joydeep Banik <jdarnab@gmail.com>
2025-01-16 16:03:54 +0600

    Add files via upload

---
+++ Week 1/Video 6 ( AVG, Sum).py
@@ -0,0 +1,27 @@
+a=1
+b=2
+c=3
+# x= (a+b)
+# y=(b/c)
+# z= (c//b)
+# # floor division
+# print (z)
+# print(c%b)
+# if a>b>c:
+#     print("X is the Largest")
+# elif b>a and b>c :
+#     print( "b is the Largest")
+# else:
+#     print("c is the Greatest")
+# A=float(input("Enter your CGPA"))
+# C= float(input ("Enter your CGPA"))
+# B= float(input ("Enter your cg"))
+# if A>B  and A>C :
+#     print ( "A has higher cgpa")
+# elif B>C and B>C :
+#     print ( "B has higher cgpa")
+# else :
+#     print ( " C has the higher cg")
+
+# print( f" Their avg cg is {(A+B+C)/3}")
+print (a|b, a & b)
